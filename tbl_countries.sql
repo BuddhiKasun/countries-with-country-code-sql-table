a5c2c7d2384a2a39e8cdc827627c69266e5147d2
buddhikasun <buddhikasun2012@gmail.com>
2017-12-12 11:16:33 +0530

    countries table

---
+++ tbl_countries.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Dec 12, 2017 at 11:15 AM
+-- Server version: 5.7.20-0ubuntu0.17.10.1
+-- PHP Version: 7.1.8-1ubuntu1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gis_db_hotel_corbets`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_countries`
+--
+
+CREATE TABLE `tbl_countries` (
+  `country_id` int(10) NOT NULL,
+  `country_code` varchar(10) NOT NULL,
+  `country_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_countries`
+--
+
+INSERT INTO `tbl_countries` (`country_id`, `country_code`, `country_name`) VALUES
+(1, 'AF', 'Afghanistan'),
+(2, 'AL', 'Albania'),
+(3, 'DZ', 'Algeria'),
+(4, 'DS', 'American Samoa'),
+(5, 'AD', 'Andorra'),
+(6, 'AI', 'Anguilla'),
+(7, 'AQ', 'Antarctica'),
+(8, 'AG', 'Antigua and Barbuda'),
+(9, 'AR', 'Argentina'),
+(10, 'AM', 'Armenia'),
+(11, 'AW', 'Aruba'),
+(12, 'AU', 'Australia'),
+(13, 'AT', 'Austria'),
+(14, 'AZ', 'Azerbaijan'),
+(15, 'BS', 'Bahamas'),
+(16, 'BH', 'Bahrain'),
+(17, 'BD', 'Bangladesh'),
+(18, 'BB', 'Barbados'),
+(19, 'BY', 'Belarus'),
+(20, 'BE', 'Belgium'),
+(21, 'BZ', 'Belize'),
+(22, 'BJ', 'Benin'),
+(23, 'BM', 'Bermuda'),
+(24, 'BT', 'Bhutan'),
+(25, 'BO', 'Bolivia'),
+(26, 'BA', 'Bosnia and Herzegovina'),
+(27, 'BW', 'Botswana'),
+(28, 'BV', 'Bouvet Island'),
+(29, 'BR', 'Brazil'),
+(30, 'IO', 'British Indian Ocean Territory'),
+(31, 'BN', 'Brunei Darussalam'),
+(32, 'BG', 'Bulgaria'),
+(33, 'BF', 'Burkina Faso'),
+(34, 'BI', 'Burundi'),
+(35, 'KH', 'Cambodia'),
+(36, 'CM', 'Cameroon'),
+(37, 'CA', 'Canada'),
+(38, 'CV', 'Cape Verde'),
+(39, 'KY', 'Cayman Islands'),
+(40, 'CF', 'Central African Republic'),
+(41, 'TD', 'Chad'),
+(42, 'CL', 'Chile'),
+(43, 'CN', 'China'),
+(44, 'CX', 'Christmas Island'),
+(45, 'CC', 'Cocos (Keeling) Islands'),
+(46, 'CO', 'Colombia'),
+(47, 'KM', 'Comoros'),
+(48, 'CG', 'Congo'),
+(49, 'CK', 'Cook Islands'),
+(50, 'CR', 'Costa Rica');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_countries`
+--
+ALTER TABLE `tbl_countries`
+  ADD PRIMARY KEY (`country_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_countries`
+--
+ALTER TABLE `tbl_countries`
+  MODIFY `country_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
